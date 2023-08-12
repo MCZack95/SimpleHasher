92356ba2b2eecec01c00c819097600ad83c78cb5
zack <yeo_zh@hotmail.com>
2023-08-12 10:57:12 +0800

    Added Hash Logic

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,31 +1,105 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.security.MessageDigest
+import java.security.Security
+
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var mExpanded by remember { mutableStateOf(false) }
+    var mSelectedText by remember { mutableStateOf("") }
+    var text by remember { mutableStateOf("") }
+
+    // Up Icon when expanded and down icon when collapsed
+    val icon = if (mExpanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column(modifier = Modifier.fillMaxSize().padding(10.dp, 10.dp, 10.dp, 10.dp),
+            horizontalAlignment = Alignment.Start) {
+            Row(modifier = Modifier.fillMaxWidth().padding(5.dp, 5.dp),
+                verticalAlignment = Alignment.Top) {
+                TextField(
+                    value = text,
+                    onValueChange = { text = it },
+                    label = { Text("Output") },
+                    modifier = Modifier.fillMaxWidth().padding(10.dp, 10.dp, 10.dp, 0.dp)
+                )
+            }
+            Row(modifier = Modifier.fillMaxWidth().padding(5.dp, 5.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center) {
+
+                Column(modifier = Modifier.padding(10.dp, 10.dp, 10.dp, 10.dp)) {
+                    OutlinedTextField(
+                        value = mSelectedText,
+                        onValueChange = { mSelectedText = it },
+                        label = {Text("Select Hash Type")},
+                        trailingIcon = {
+                            Icon(icon,"contentDescription",
+                                Modifier.clickable { mExpanded = !mExpanded })
+                        },
+                        modifier = Modifier.width(250.dp),
+                        readOnly = true
+                    )
+                    DropdownMenu(
+                        expanded = mExpanded,
+                        onDismissRequest = { mExpanded = false }
+                    ) {
+                        val hashList = getHashOptions()
+                        hashList.forEach { label ->
+                            DropdownMenuItem(onClick = {
+                                mSelectedText = label
+                                mExpanded = false
+                            }) {
+                                Text(text = label)
+                            }
+                        }
+                    }
+                }
+                Column(modifier = Modifier.padding(10.dp, 10.dp, 10.dp, 10.dp)) {
+                    Button(onClick = {
+                        text = generateHash(text, mSelectedText)
+                    }) {
+                        Text("Generate Hash")
+                    }
+                }
+            }
         }
     }
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(onCloseRequest = ::exitApplication, title = "Simple Hash Generator") {
         App()
     }
+}
+
+fun getHashOptions(): List<String> {
+    return Security.getAlgorithms("MessageDigest").toList()
+}
+
+fun generateHash(text: String, hashType: String): String {
+    val input = text.toByteArray()
+    val md = MessageDigest.getInstance(hashType)
+    val bytes = md.digest(input)
+    var hash = ""
+    bytes.forEach { byte ->
+        hash += String.format("%02X", byte)
+    }
+    return hash
 }
